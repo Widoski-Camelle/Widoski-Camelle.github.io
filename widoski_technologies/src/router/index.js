7be8af991142810f58f28ecb356e86215238dac8
Widoski <camelle-widoski.houndagba@epitech.eu>
2023-01-05 11:46:55 +0100

    Portfolio-v1

---
+++ widoski_technologies/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Accueil from '../views/Accueil.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Accueil',
+    component: Accueil
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
